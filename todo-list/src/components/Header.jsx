adabb7f43b37b0f978cc4a7d5f231a3440ed7d70
thekidisalright <dev.alexandresilva@gmail.com>
2024-03-02 18:21:30 -0300

    Funcionalidade de temas

--- todo-list/src/components/Header.jsx
+++ todo-list/src/components/Header.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
 function Header() {
+  function toggleTema() {
+    const body = document.querySelector('.App');
+    body.classList.toggle("vol2");
+    if(body.classList.contains("vol2")) {
+      localStorage.setItem('tema', 'vol2');
+    } else {
+      localStorage.setItem('tema', '');
+    }
+  }
   return (
     <header className="header">
       <h1 className="brand">TodoList</h1>
       <label className="switch">
-        <input className="toggle" type="checkbox" />
+        <input className="toggle" type="checkbox" onClick={toggleTema}/>
         <span className="slider"></span>
         <span className="cardSide"></span>
       </label>
