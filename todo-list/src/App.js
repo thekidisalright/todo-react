8a9053c3bb2f41874092d70ceb3effab51569620
thekidisalright <dev.alexandresilva@gmail.com>
2024-03-02 16:51:38 -0300

    Adicionou elementos do todo

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Recuperar a Espada Sagrada",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Treinar artes marciais com Pai Mei",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Matar Bill",
+      isCompleted: false,
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <div key={todo.id} className="todo">
+            <div className="content">
+              <label className="checkBox">
+                <input className="ch1" type="checkbox" />
+                <div className="transition"></div>
+              </label>
+              <span>{todo.text}</span>
+            </div>
+            <button className="delete">x</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
