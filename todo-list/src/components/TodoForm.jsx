ecf86a1e13c0fec98e2709c723504462d6709112
thekidisalright <dev.alexandresilva@gmail.com>
2024-03-02 17:11:40 -0300

    Adicionar componente TodoForm

---
+++ todo-list/src/components/TodoForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoForm() {
+  return (
+    <div className="todo-form">
+      <input type="text" placeholder="Crie uma nova tarefa" />
+      <button>Criar</button>
+    </div>
+  );
+}
+
+export default TodoForm;
